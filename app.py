69c8cb4f7c7a29b259deab48279bbc834fa4a66d
Gaurav Reddy <gauravreddy008@gmail.com>
2023-11-07 10:50:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ import base64
 valid_image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"]
 
 service_account= {
-  "type"= st.secrets['firebase']['type'],
-  "project_id"= st.secrets['firebase']['project_id'],
-  "private_key_id"= st.secrets['firebase']['project_key_id'],
-  "private_key"= st.secrets['firebase']['private_key'],
-  "client_email"= st.secrets['firebase']['client_email'],
-  "client_id"= st.secrets['firebase']['client_id'],
-  "auth_uri"= st.secrets['firebase']['auth_uri'],
-  "token_uri"= st.secrets['firebase']['token_uri'],
-  "auth_provider_x509_cert_url"= st.secrets['firebase']['auth_provider_x509_cert_url'],
-  "client_x509_cert_url"= st.secrets['firebase']['client_x509_cert_url'],
-  "universe_domain"= st.secrets['firebase']['universe_domain']
+  "type" : st.secrets['firebase']['type'],
+  "project_id" : st.secrets['firebase']['project_id'],
+  "private_key_id" : st.secrets['firebase']['project_key_id'],
+  "private_key" : st.secrets['firebase']['private_key'],
+  "client_email" : st.secrets['firebase']['client_email'],
+  "client_id" : st.secrets['firebase']['client_id'],
+  "auth_uri" : st.secrets['firebase']['auth_uri'],
+  "token_uri" : st.secrets['firebase']['token_uri'],
+  "auth_provider_x509_cert_url" : st.secrets['firebase']['auth_provider_x509_cert_url'],
+  "client_x509_cert_url" : st.secrets['firebase']['client_x509_cert_url'],
+  "universe_domain" : st.secrets['firebase']['universe_domain']
 }
 
 def perform_function(messages):
